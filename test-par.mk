2ab532a9aa12b613fde58db10337c01121488f57
Linnea Ingmar <lingmar@kth.se>
2021-06-11 16:26:27 +0200

    Fix parse error in test-par.mk

--- test-par.mk
+++ test-par.mk
@@ -2,6 +2,8 @@
 compile_files =
 compile_files += stdlib/multicore/atomic.mc
 compile_files += stdlib/multicore/thread.mc
+compile_files += stdlib/multicore/channel.mc
+compile_files += stdlib/multicore/thread-pool.mc
 
 all: ${compile_files}
 
